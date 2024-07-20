db868eb35f16273209c30cc188e4311de046fe62
xochimanque <xochimanque@protonmail.com>
2024-07-21 00:30:10 +0200

    Updated Javascript y nuevo html

--- JavaScript/interactivity.js
+++ JavaScript/interactivity.js
@@ -2,9 +2,10 @@
 document.getElementById('defuriganify').addEventListener('click', function () {
     var rtElements = document.querySelectorAll('rt');
     var Button = document.getElementById('defuriganify');
+    var togButton = document.getElementById('toggleButton');
     rtElements.forEach(function (rtElement) {
         var currentColor = rgbToHex(getComputedStyle(rtElement).color);
-        var currentButtonColor = rgbToHex(getComputedStyle(Button).color);
+        var currentButtonColor = rgbToHex(getComputedStyle(togButton).color);
         if (currentColor === '#273800') {
             rtElement.style.color = 'transparent';
             Button.style.color = '#7c745f';
@@ -12,9 +13,17 @@ document.getElementById('defuriganify').addEventListener('click', function () {
             rtElement.style.color = '#273800';
             Button.style.color = '#273800';
         }
+        if (currentButtonColor === '#7c745f' || 'maroon') {
+            togButton.style.color ='#273800';
+        } else{
+            togButton.style.color ='#7c745f';
+        }
 
     });
 });
+
+
+
 
 function rgbToHex(rgb) {
     var result = rgb.match(/\d+/g).map(function (x) {
@@ -23,3 +32,47 @@ function rgbToHex(rgb) {
     });
     return '#' + result.join('');
 }
+
+let timeout;
+let timerActive= false;
+const elementsToHide = document.querySelectorAll('header, footer, .day');
+const timeToDisappear = 3 * 1000;
+
+function resetTimer() {
+    if (!timerActive) return;
+    clearTimeout(timeout);
+    elementsToHide.forEach(element => element.classList.remove('hidden'));
+    document.body.classList.remove('hidden-cursor');
+    timeout = setTimeout(() => {
+        elementsToHide.forEach(element => element.classList.add('hidden'));
+        document.body.classList.add('hidden-cursor');
+    }, timeToDisappear);
+}
+
+function toggleTimer() {
+    timerActive = !timerActive;
+    if (timerActive) {
+        resetTimer();
+    } else {
+        clearTimeout(timeout);
+        elementsToHide.forEach(element => element.classList.remove('hidden'));
+        document.body.classList.remove('hidden-cursor');
+    }
+}
+document.getElementById('toggleButton').addEventListener('click', toggleTimer);
+document.addEventListener('mousemove', resetTimer);
+//document.addEventListener('keydown', resetTimer);
+document.addEventListener('touchstart', resetTimer);
+document.addEventListener('touchmove', resetTimer);
+//Change Focus Button colour
+document.getElementById('toggleButton').addEventListener('click', function () {
+    var togButton = document.getElementById('toggleButton');
+    if (timerActive) {
+        togButton.style.color ='#273800';
+    } else{
+        togButton.style.color ='#7c745f';
+    }
+
+});
+// Initialize the timer
+resetTimer();
